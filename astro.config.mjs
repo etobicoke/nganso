d64a5a798962b1ab2ed295cae7a60f4729651eb7
G. Nganso <nganso@gmail.com>
2022-08-27 17:43:18 -0400

    refac: removed "base" setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://etobicoke.github.io',
-    base: '/nganso'
+    site: 'https://etobicoke.github.io'
 });
