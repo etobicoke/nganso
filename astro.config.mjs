64c7f7da04121b83fef7ae1115a6d0358090db3a
G. Nganso <nganso@gmail.com>
2022-09-01 13:34:28 -0400

    fix: adding repo name to site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://etobicoke.github.io'
+    site: 'https://etobicoke.github.io/nganso/'
 });
