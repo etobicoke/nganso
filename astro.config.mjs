e4b19a702c3ff5102c05af7e174784864810be53
G. Nganso <nganso@gmail.com>
2022-09-01 13:42:23 -0400

    fix: removed trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://etobicoke.github.io/nganso/'
+    site: 'https://etobicoke.github.io/nganso'
 });
