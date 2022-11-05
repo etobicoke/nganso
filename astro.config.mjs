2299160c351cf3b032572e1541aa2373a2e3053b
G. Nganso <nganso@gmail.com>
2022-11-04 21:48:39 -0400

    feat: prism theme setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://etobicoke.github.io'
+    site: 'https://etobicoke.github.io',
+    markdown: {
+        // Can be 'shiki' (default), 'prism' or false to disable highlighting
+        syntaxHighlight: 'prism',
+    },
 });
